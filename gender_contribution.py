35262157a949c97791d115592633d3a8367ff0e6
Shouvik-Dasgupta <95506716+Shouvik-Dasgupta@users.noreply.github.com>
2022-03-30 21:43:18 +0200

    Update gender_contribution.py

--- gender_contribution.py
+++ gender_contribution.py
@@ -6,143 +6,144 @@ from vega_datasets import data
 from streamlit_vega_lite import vega_lite_component, altair_component
 import time
 
-st.write('# Summer Games')
+def app():
+    st.write('# Summer Games')
 
-@st.cache
-def loadData():
-    return pd.read_csv('df_summer.csv')
+    @st.cache
+    def loadData():
+        return pd.read_csv('data/df_summer.csv')
 
-df_map_summer = loadData()
-
-
-sportSet = sorted(set(df_map_summer['Sport'].unique()))
-
-def createAthletesDF(scope):
-    # Filter DF by sport and teams that won a medal to reduce amount of data
-    filter = df_map_summer[df_map_summer['Sport'] == str(scope)]
-    participants = filter['Team'].unique()
-    years = filter['Year'].unique()
+    df_map_summer = loadData()
 
 
-    liste = []
+    sportSet = sorted(set(df_map_summer['Sport'].unique()))
 
-    # iterate through all participating countries and filter genders out
-    for participant in participants:
-        for year in years: 
-            Male = 0
-            Female = 0
-            countryFilter = filter[filter['Team'] == str(participant)]
-            yearFilter = countryFilter[countryFilter['Year'] == year]
-            Male = len(yearFilter[yearFilter['Sex']=='M'])
-            Female = len(yearFilter[yearFilter['Sex']=='F'])
-            Sum = Male + Female
-
-            MaleRow = [participant, year, Male, 'Male', Sum, 1]
-            FemaleRow = [participant, year, Female, 'Female', Sum, 2]
-
-            liste.append(MaleRow)
-            liste.append(FemaleRow)
-
-    AthletesDF = pd.DataFrame(liste)
-    AthletesDF = AthletesDF.rename(columns={0: "Country", 1: "Year", 2: "Amount", 3: 'Gender', 4: 'Sum', 5: 'Ordering'})
-    sort = AthletesDF.sort_values('Sum', ascending=False)
-    sortOrder = sort['Country'].unique()
-
-    return AthletesDF, sortOrder
+    def createAthletesDF(scope):
+        # Filter DF by sport and teams that won a medal to reduce amount of data
+        filter = df_map_summer[df_map_summer['Sport'] == str(scope)]
+        participants = filter['Team'].unique()
+        years = filter['Year'].unique()
 
 
-st.markdown("## Breakdown of Medals Won by Genders in Country and Sport")
+        liste = []
 
-option = st.selectbox('Select the sport',(sportSet))
+        # iterate through all participating countries and filter genders out
+        for participant in participants:
+            for year in years: 
+                Male = 0
+                Female = 0
+                countryFilter = filter[filter['Team'] == str(participant)]
+                yearFilter = countryFilter[countryFilter['Year'] == year]
+                Male = len(yearFilter[yearFilter['Sex']=='M'])
+                Female = len(yearFilter[yearFilter['Sex']=='F'])
+                Sum = Male + Female
+
+                MaleRow = [participant, year, Male, 'Male', Sum, 1]
+                FemaleRow = [participant, year, Female, 'Female', Sum, 2]
+
+                liste.append(MaleRow)
+                liste.append(FemaleRow)
+
+        AthletesDF = pd.DataFrame(liste)
+        AthletesDF = AthletesDF.rename(columns={0: "Country", 1: "Year", 2: "Amount", 3: 'Gender', 4: 'Sum', 5: 'Ordering'})
+        sort = AthletesDF.sort_values('Sum', ascending=False)
+        sortOrder = sort['Country'].unique()
+
+        return AthletesDF, sortOrder
 
 
-data,sortOrder = createAthletesDF(option)
+    st.markdown("## Breakdown of Medals Won by Genders in Country and Sport")
 
-date_slider = alt.binding_range(min=min(data['Year']), max=max(data['Year']), step=4)
-slider_selection = alt.selection_single(bind=date_slider, fields=['Year'], name = 'Slider', init={'Year': 1896})
+    option = st.selectbox('Select the sport',(sportSet))
 
 
-bars = alt.Chart(data).mark_bar().encode(
-    x=alt.X('Amount:O', stack='zero'),
-    y=alt.Y('Country:N', sort = sortOrder),
-    color=alt.Color('Gender:N')
-).add_selection(slider_selection).transform_filter(slider_selection)
+    data,sortOrder = createAthletesDF(option)
 
-text = alt.Chart(data).mark_text(dx=-15, dy=3, color='white').encode(
-    x=alt.X('Amount:O', stack='zero'),
-    y=alt.Y('Country:N'),
-    detail='Gender:N'
-)
+    date_slider = alt.binding_range(min=min(data['Year']), max=max(data['Year']), step=4)
+    slider_selection = alt.selection_single(bind=date_slider, fields=['Year'], name = 'Slider', init={'Year': 1896})
 
-st.write(bars + text)
+
+    bars = alt.Chart(data).mark_bar().encode(
+        x=alt.X('Amount:O', stack='zero'),
+        y=alt.Y('Country:N', sort = sortOrder),
+        color=alt.Color('Gender:N')
+    ).add_selection(slider_selection).transform_filter(slider_selection)
+
+    text = alt.Chart(data).mark_text(dx=-15, dy=3, color='white').encode(
+        x=alt.X('Amount:O', stack='zero'),
+        y=alt.Y('Country:N'),
+        detail='Gender:N'
+    )
+
+    st.write(bars + text)
 
 #####################################################################################################################################
 
-st.write('# Winter Games')
-@st.cache
-def loadData():
-    return pd.read_csv('df_winter.csv')
+    st.write('# Winter Games')
+    @st.cache
+    def loadData():
+        return pd.read_csv('data/df_winter.csv')
 
-df_map_winter = loadData()
-
-
-sportSet = sorted(set(df_map_winter['Sport'].unique()))
-
-def createAthletesDF(scope):
-    # Filter DF by sport and teams that won a medal to reduce amount of data
-    filter = df_map_winter[df_map_winter['Sport'] == str(scope)]
-    participants = filter['Team'].unique()
-    years = filter['Year'].unique()
+    df_map_winter = loadData()
 
 
-    liste = []
+    sportSet = sorted(set(df_map_winter['Sport'].unique()))
 
-    # iterate through all participating countries and filter genders out
-    for participant in participants:
-        for year in years: 
-            Male = 0
-            Female = 0
-            countryFilter = filter[filter['Team'] == str(participant)]
-            yearFilter = countryFilter[countryFilter['Year'] == year]
-            Male = len(yearFilter[yearFilter['Sex']=='M'])
-            Female = len(yearFilter[yearFilter['Sex']=='F'])
-            Sum = Male + Female
-
-            MaleRow = [participant, year, Male, 'Male', Sum, 1]
-            FemaleRow = [participant, year, Female, 'Female', Sum, 2]
-
-            liste.append(MaleRow)
-            liste.append(FemaleRow)
-
-    AthletesDF = pd.DataFrame(liste)
-    AthletesDF = AthletesDF.rename(columns={0: "Country", 1: "Year", 2: "Amount", 3: 'Gender', 4: 'Sum', 5: 'Ordering'})
-    sort = AthletesDF.sort_values('Sum', ascending=False)
-    sortOrder = sort['Country'].unique()
-
-    return AthletesDF, sortOrder
+    def createAthletesDF(scope):
+        # Filter DF by sport and teams that won a medal to reduce amount of data
+        filter = df_map_winter[df_map_winter['Sport'] == str(scope)]
+        participants = filter['Team'].unique()
+        years = filter['Year'].unique()
 
 
-st.markdown("## Breakdown of Medals Won by Genders in Country and Sport")
+        liste = []
 
-option = st.selectbox('Select the sport',(sportSet))
+        # iterate through all participating countries and filter genders out
+        for participant in participants:
+            for year in years: 
+                Male = 0
+                Female = 0
+                countryFilter = filter[filter['Team'] == str(participant)]
+                yearFilter = countryFilter[countryFilter['Year'] == year]
+                Male = len(yearFilter[yearFilter['Sex']=='M'])
+                Female = len(yearFilter[yearFilter['Sex']=='F'])
+                Sum = Male + Female
+
+                MaleRow = [participant, year, Male, 'Male', Sum, 1]
+                FemaleRow = [participant, year, Female, 'Female', Sum, 2]
+
+                liste.append(MaleRow)
+                liste.append(FemaleRow)
+
+        AthletesDF = pd.DataFrame(liste)
+        AthletesDF = AthletesDF.rename(columns={0: "Country", 1: "Year", 2: "Amount", 3: 'Gender', 4: 'Sum', 5: 'Ordering'})
+        sort = AthletesDF.sort_values('Sum', ascending=False)
+        sortOrder = sort['Country'].unique()
+
+        return AthletesDF, sortOrder
 
 
-data,sortOrder = createAthletesDF(option)
+    st.markdown("## Breakdown of Medals Won by Genders in Country and Sport")
 
-date_slider = alt.binding_range(min=min(data['Year']), max=max(data['Year']), step=4)
-slider_selection = alt.selection_single(bind=date_slider, fields=['Year'], name = 'Slider', init={'Year': 1924})
+    option = st.selectbox('Select the sport',(sportSet))
 
 
-bars = alt.Chart(data).mark_bar().encode(
-    x=alt.X('Amount:O', stack='zero'),
-    y=alt.Y('Country:N', sort = sortOrder),
-    color=alt.Color('Gender:N')
-).add_selection(slider_selection).transform_filter(slider_selection)
+    data,sortOrder = createAthletesDF(option)
 
-text = alt.Chart(data).mark_text(dx=-15, dy=3, color='white').encode(
-    x=alt.X('Amount:O', stack='zero'),
-    y=alt.Y('Country:N'),
-    detail='Gender:N'
-)
+    date_slider = alt.binding_range(min=min(data['Year']), max=max(data['Year']), step=4)
+    slider_selection = alt.selection_single(bind=date_slider, fields=['Year'], name = 'Slider', init={'Year': 1924})
 
-st.write(bars + text)
+
+    bars = alt.Chart(data).mark_bar().encode(
+        x=alt.X('Amount:O', stack='zero'),
+        y=alt.Y('Country:N', sort = sortOrder),
+        color=alt.Color('Gender:N')
+    ).add_selection(slider_selection).transform_filter(slider_selection)
+
+    text = alt.Chart(data).mark_text(dx=-15, dy=3, color='white').encode(
+        x=alt.X('Amount:O', stack='zero'),
+        y=alt.Y('Country:N'),
+        detail='Gender:N'
+    )
+
+    st.write(bars + text)
